c399fe6ee25051c1a296c36c4bd89d4b7e81ba12
jadorks <adorkorj@gmail.com>
2023-03-23 00:13:11 +0000

    Add initial layout

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Navbar</div>
+    </div>
+  );
+}
