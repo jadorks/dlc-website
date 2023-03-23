27cffea5688139698f4b233cef48acaa4f89af16
jadorks <adorkorj@gmail.com>
2023-03-23 00:13:11 +0000

    Add fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        alien_encounters_solid: ["Alien-Encounters-Solid", "sans-serif"],
+        alien_encounters: ["Alien-Encounters", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"]
+      },
+    },
   },
   plugins: [],
-}
+};
