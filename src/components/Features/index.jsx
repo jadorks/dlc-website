cae0217719789bd04eb17eecb34ffaae94a60b6b
jadorks <adorkorj@gmail.com>
2023-03-23 02:41:36 +0000

    Update text for features

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -7,28 +7,28 @@ export default function Features() {
       image: "/images/features/feature1.png",
       title: "Voice Automation",
       description:
-        "This technology will be an extension that also allows users to interact and navigate with decentralized finance (DeFi) protocols as well as blockchains.",
+        "Deep Learning Chain utilizes voice-automation technology, which allows users to interact with the network using natural language. This technology is augmented by artificial intelligence (AI), which makes it easier for users to interact with the network and perform tasks such as creating transactions, checking balances, and more.",
       className: "lg:object-[32%]",
     },
     {
       image: "/images/features/feature2.png",
       title: "ChatGPT Integration",
       description:
-        "This technology will be an extension that also allows users to interact and navigate with decentralized finance (DeFi) protocols as well as blockchains.",
+        "Deep Learning Chain leverages OpenAI's ChatGPT to allow natural interaction with the blockchain. It augments Deep Learning Chain's voice automation and helps understand the commands that are issued by users.",
       className: "lg:object-[70%]",
     },
     {
       image: "/images/features/feature3.png",
       title: "Security",
       description:
-        "This technology will be an extension that also allows users to interact and navigate with decentralized finance (DeFi) protocols as well as blockchains.",
+        "Deep Learning Chain is designed to be highly secure. The Proof-of-Authority consensus mechanism ensures that only validators that are chosen by the network’s users are allowed to validate transactions. The network also utilizes advanced cryptography and other security measures to ensure that user data is safe and secure.",
       className: "lg:object-[70%]",
     },
     {
       image: "/images/features/feature4.png",
       title: "POA Consensus",
       description:
-        "This technology will be an extension that also allows users to interact and navigate with decentralized finance (DeFi) protocols as well as blockchains.",
+        "Deep Learning Chain uses a Proof-of-Authority consensus mechanism. Proof of Authority (PoA) is a consensus mechanism that is based on a set of predetermined “validators” who are responsible for validating blocks on the network. These validators are selected based on their reputation and trustworthiness, and they are required to adhere to certain criteria in order to be accepted as a validator.",
       className: "lg:object-[100%]",
     },
   ];
